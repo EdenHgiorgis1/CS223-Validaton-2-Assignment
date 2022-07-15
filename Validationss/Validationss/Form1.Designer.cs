7ca3cd52e508caec37a33c8b26b3e7751ac560a4
EdenHGiorgis <edenhgiorgis21@gmail.com>
2022-07-15 21:45:00 +0300

    Validation

---
+++ Validationss/Validationss/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Validationss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.txt_Number = new System.Windows.Forms.TextBox();
+            this.txt_In = new System.Windows.Forms.TextBox();
+            this.txt_ObjectName = new System.Windows.Forms.TextBox();
+            this.txt_Count = new System.Windows.Forms.TextBox();
+            this.txt_Price = new System.Windows.Forms.TextBox();
+            this.dtp_datepicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_PhoneNo = new System.Windows.Forms.TextBox();
+            this.dgvShowProducts = new System.Windows.Forms.DataGridView();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShowProducts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(298, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(525, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Inventory Number";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(75, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Object Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(75, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Count";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(350, 219);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 18);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Price";
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add.Location = new System.Drawing.Point(713, 331);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
+            this.btn_Add.TabIndex = 6;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Cancel.Location = new System.Drawing.Point(713, 391);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_Cancel.TabIndex = 7;
+            this.btn_Cancel.Text = "Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // txt_Number
+            // 
+            this.txt_Number.Location = new System.Drawing.Point(78, 75);
+            this.txt_Number.Name = "txt_Number";
+            this.txt_Number.Size = new System.Drawing.Size(126, 20);
+            this.txt_Number.TabIndex = 8;
+            this.txt_Number.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_In
+            // 
+            this.txt_In.Location = new System.Drawing.Point(528, 78);
+            this.txt_In.Name = "txt_In";
+            this.txt_In.Size = new System.Drawing.Size(190, 20);
+            this.txt_In.TabIndex = 10;
+            // 
+            // txt_ObjectName
+            // 
+            this.txt_ObjectName.Location = new System.Drawing.Point(78, 169);
+            this.txt_ObjectName.Name = "txt_ObjectName";
+            this.txt_ObjectName.Size = new System.Drawing.Size(380, 20);
+            this.txt_ObjectName.TabIndex = 11;
+            this.txt_ObjectName.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // txt_Count
+            // 
+            this.txt_Count.Location = new System.Drawing.Point(78, 265);
+            this.txt_Count.Name = "txt_Count";
+            this.txt_Count.Size = new System.Drawing.Size(100, 20);
+            this.txt_Count.TabIndex = 12;
+            // 
+            // txt_Price
+            // 
+            this.txt_Price.Location = new System.Drawing.Point(353, 265);
+            this.txt_Price.Name = "txt_Price";
+            this.txt_Price.Size = new System.Drawing.Size(100, 20);
+            this.txt_Price.TabIndex = 13;
+            this.txt_Price.TextChanged += new System.EventHandler(this.txt_Price_TextChanged);
+            // 
+            // dtp_datepicker1
+            // 
+            this.dtp_datepicker1.Location = new System.Drawing.Point(287, 75);
+            this.dtp_datepicker1.Name = "dtp_datepicker1";
+            this.dtp_datepicker1.Size = new System.Drawing.Size(200, 20);
+            this.dtp_datepicker1.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(525, 132);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(120, 18);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Phone Number";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // txt_PhoneNo
+            // 
+            this.txt_PhoneNo.Location = new System.Drawing.Point(528, 169);
+            this.txt_PhoneNo.Name = "txt_PhoneNo";
+            this.txt_PhoneNo.Size = new System.Drawing.Size(190, 20);
+            this.txt_PhoneNo.TabIndex = 16;
+            // 
+            // dgvShowProducts
+            // 
+            this.dgvShowProducts.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgvShowProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvShowProducts.GridColor = System.Drawing.SystemColors.InactiveCaption;
+            this.dgvShowProducts.Location = new System.Drawing.Point(34, 319);
+            this.dgvShowProducts.Name = "dgvShowProducts";
+            this.dgvShowProducts.Size = new System.Drawing.Size(647, 107);
+            this.dgvShowProducts.TabIndex = 17;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvShowProducts);
+            this.Controls.Add(this.txt_PhoneNo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dtp_datepicker1);
+            this.Controls.Add(this.txt_Price);
+            this.Controls.Add(this.txt_Count);
+            this.Controls.Add(this.txt_ObjectName);
+            this.Controls.Add(this.txt_In);
+            this.Controls.Add(this.txt_Number);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShowProducts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.TextBox txt_Number;
+        private System.Windows.Forms.TextBox txt_In;
+        private System.Windows.Forms.TextBox txt_ObjectName;
+        private System.Windows.Forms.TextBox txt_Count;
+        private System.Windows.Forms.TextBox txt_Price;
+        private System.Windows.Forms.DateTimePicker dtp_datepicker1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_PhoneNo;
+        private System.Windows.Forms.DataGridView dgvShowProducts;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
